78f2791197215adce6bcfbd90cdc2a9bb5ec53f1
SUBHAKARNI8818 <92645629+SUBHAKARNI8818@users.noreply.github.com>
2022-02-21 20:45:03 +0530

    area.py

---
+++ area.py
@@ -0,0 +1,5 @@
+#To enter the radius of the circle
+Radius = float(input ("Input the radius of the circle : "))
+Area = str(3.14 * Radius**2);
+print ("The area of the circle with radius " + str(Radius) + " is: " + Area)
+
