96f792967a613a80c7b6acf1a1811dab02c1a5ba
SUBHAKARNI8818 <92645629+SUBHAKARNI8818@users.noreply.github.com>
2022-02-27 12:06:36 +0530

    print_positivenumbers.py

---
+++ print_positivenumbers.py
@@ -0,0 +1,10 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+print("The positive numbers in the list1 are : ")
+for num in list1:
+    if num >= 0:
+       print(num, end = " ")
+print("\nThe positive numbers in the list2 are : ")
+for num in list2:
+    if num >= 0:
+       print(num, end = " ")
