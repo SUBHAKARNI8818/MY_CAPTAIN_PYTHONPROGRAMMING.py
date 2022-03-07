5770355f01c8d46941e7865c78c98e4deaf9e52f
SUBHAKARNI8818 <92645629+SUBHAKARNI8818@users.noreply.github.com>
2022-03-07 21:51:07 +0530

    Update Extension.py

--- Extension.py
+++ Extension.py
@@ -1,4 +1,5 @@
 #To enter the extension of the filename 
 file_name = input("Enter the filename : ")
-file_ext = file_name.split(".")
-print("The extension of the file is : " + repr(file_ext[-1]))
+file_ext =  input("Enter the filextension : ")
+print("The name of the file : ", file_name,".",file_ext )
+print("The extension of the file is : ", file_ext)
